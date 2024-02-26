17f5768058c6eb3c172efb35c5210f6d0783f3df
notsahill <camlfw20002@glbitm.ac.in>
2024-02-26 23:36:55 +0530

    Updated cors

--- backend/dist/controllers/chat-controllers.js
+++ backend/dist/controllers/chat-controllers.js
@@ -21,7 +21,7 @@ export const generateChatCompletion = async (req, res, next) => {
         const openai = new OpenAIApi(config);
         // get latest response
         const chatResponse = await openai.createChatCompletion({
-            model: "gpt-4",
+            model: "gpt-3.5-turbo",
             messages: chats,
         });
         user.chats.push(chatResponse.data.choices[0].message);
--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const loginUser = async (email: string, password: string) => {
-  const res = await axios.post("/user/login", { email, password });
+  const res = await axios.post("/user/login", {email,password});
   if (res.status !== 200) {
     throw new Error("Unable to login");
   }
